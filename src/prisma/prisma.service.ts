a2ddd6a287af85491e627b8bdbce4651c8913b9f
Satvik <satviksachan02@gmail.com>
2026-01-17 18:59:55 +0530

    fix:prisma constructor added for adapter update to be able to use postgres

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,16 @@
 import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+
+constructor() {
+    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    super({ adapter: pool });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
